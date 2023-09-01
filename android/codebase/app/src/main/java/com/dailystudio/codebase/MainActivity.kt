0ff37dc47ff30cf48a05ddd877519ae3d6c94019
Nan YE <dailystudio2010@gmail.com>
2023-09-01 21:53:04 +0800

    [android][DEV]: add compose project;

--- android/codebase/app/src/main/java/com/dailystudio/codebase/MainActivity.kt
+++ android/codebase/app/src/main/java/com/dailystudio/codebase/MainActivity.kt
@@ -2,6 +2,8 @@ package com.dailystudio.codebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.dailystudio.devbricksx.development.Logger
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        val topBar: Toolbar? = findViewById(R.id.topAppBar)
+        Logger.debug("topBar: $topBar")
+
+        topBar?.let {
+            setSupportActionBar(it)
+        }
+
+    }
+
 }
