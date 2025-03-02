57547d1e40f64d129b57c5932f9000a7cbced107
Nan YE <dailystudio2010@gmail.com>
2025-03-02 19:33:13 +0800

    [android][DEV]: update vd-tool script to support size adjustment;

--- android/scripts/vd-tool/vd-tool.sh
+++ android/scripts/vd-tool/vd-tool.sh
@@ -9,8 +9,8 @@ function print_usage {
   echo "    This script will convert standard SVG image to an Android Vector Drawable"
   echo ""
   echo "    -i INPUT_FILE:              the SVG File to be converted"
-  echo "    -o OUTPUT_FILE:             the output Vector Drawable"
-  echo "    -d Dimension:               specify the dimension of the output"
+  echo "    -o OUTPUT:                  the output file / directory to save the converted Vector Drawable"
+  echo "    -d Dimension:               specify the dimension of the output, e.g. 32 or 32x32"
   echo "    -h:                         display this message"
   echo
 }
@@ -21,7 +21,7 @@ function exit_abnormal {
 }
 
 input_file=""
-output_file=""
+output=""
 dimension=""
 
 while getopts :i:o:d:hH opt; do
@@ -30,7 +30,7 @@ while getopts :i:o:d:hH opt; do
       input_file=${OPTARG}
       ;;
     o)
-      output_file=${OPTARG}
+      output=${OPTARG}
       ;;
     d)
       dimension=${OPTARG}
@@ -62,7 +62,54 @@ if [ -z "${input_file}" ]; then
     exit_abnormal
 fi
 
+width=32
+height=32
+if [ -n "$dimension" ]; then
+  if [[ "$dimension" =~ ^[0-9]+$ || "$dimension" =~ ^[0-9]+x[0-9]+$ ]]; then
+    if [[ "$dimension" =~ ^([0-9]+)x([0-9]+)$ ]]; then
+      width="${BASH_REMATCH[1]}"
+      height="${BASH_REMATCH[2]}"
+    else
+      width="$dimension"
+      height="$dimension"
+    fi
+  else
+    echo "[ERROR] Invalid format for -d. Expected number or NxN format (e.g., 32 or 32x32)."
+    exit 1
+  fi
+fi
+
+temp_dir=$(mktemp -d)
+
+input_fname=$(basename -- "${input_file}")
+input_dir=$(dirname -- "${input_file}")
+output_fname="${input_fname%.*}.xml"
+output_file_tmp=${temp_dir}/${output_fname}
+
+if [[ ! -d "${temp_dir}" ]]; then
+  echo "[ERROR] Failed to create a temporary directory."
+  exit 1
+fi
+
 ${JAVA_CMD} -jar vector-drawable-tool.jar \
-  -i "${input_file}" \
-  -o "${output_file}" \
-  -c
+  -in "${input_file}" \
+  -out "${temp_dir}" \
+  -widthDp "${width}" \
+  -heightDp "${height}" \
+  -c \
+  >/dev/null 2>&1
+
+output_file="${input_dir}/${output_fname}"
+if [ -n "$output" ]; then
+    if [ -d "${output}" ]; then
+        echo "${output} is a directory."
+        output_file=${output}/${output_fname}
+    else
+        output_file=${output}
+    fi
+else
+    echo "Output is not set. Save in the same directory as ${input_file}"
+fi
+
+cp "${output_file_tmp}" "${output_file}"
+
